4e0624e57b063d4b2dbb97535c6dee700e99b05a
Charlie Fish <fishcharlie@me.com>
2023-12-15 20:11:36 -0700

    Adding Reason tests

---
+++ src/types/Reason.test.ts
@@ -0,0 +1,64 @@
+import { Reason } from "./Reason";
+
+describe("parts()", () => {
+	const tests = [
+		["WX:Fog", ["WX", "Fog"]],
+		["WX:Low Ceilings", ["WX", "Low Ceilings"]],
+		["WX:Thunderstorms", ["WX", "Thunderstorms"]],
+		["RWY:Noise Abatement", ["RWY", "Noise Abatement"]],
+		["RWY:Maintenance", ["RWY", "Maintenance"]],
+		["RWY:Construction", ["RWY", "Construction"]],
+		["TM Initiatives:MIT:VOL", ["TM Initiatives", "MIT", "VOL"]],
+		["TM Initiatives:MIT:WX", ["TM Initiatives", "MIT", "WX"]],
+		["VOL:Compacted Demand", ["VOL", "Compacted Demand"]],
+		["VOL:Multi-taxi", ["VOL", "Multi-taxi"]],
+		["VOL:Volume", ["VOL", "Volume"]]
+	];
+
+	test.each(tests)("parts(%p) === %p", (input, expected) => {
+		const reason = new Reason(input as string);
+		expect(reason.parts()).toStrictEqual(expected);
+	});
+});
+
+describe("parentType()", () => {
+	const tests = [
+		["WX:Fog", "WX"],
+		["WX:Low Ceilings", "WX"],
+		["WX:Thunderstorms", "WX"],
+		["RWY:Noise Abatement", "RWY"],
+		["RWY:Maintenance", "RWY"],
+		["RWY:Construction", "RWY"],
+		["TM Initiatives:MIT:VOL", "TM Initiatives"],
+		["TM Initiatives:MIT:WX", "TM Initiatives"],
+		["VOL:Compacted Demand", "VOL"],
+		["VOL:Multi-taxi", "VOL"],
+		["VOL:Volume", "VOL"]
+	];
+
+	test.each(tests)("parts(%p) === %p", (input, expected) => {
+		const reason = new Reason(input as string);
+		expect(reason.parentType()).toStrictEqual(expected);
+	});
+});
+
+describe("toString()", () => {
+	const tests = [
+		["WX:Fog", "fog"],
+		["WX:Low Ceilings", "low ceilings"],
+		["WX:Thunderstorms", "thunderstorms"],
+		["RWY:Noise Abatement", "noise reduction measures"],
+		["RWY:Maintenance", "runway maintenance"],
+		["RWY:Construction", "runway construction"],
+		["TM Initiatives:MIT:VOL", "traffic management initiatives"],
+		["TM Initiatives:MIT:WX", "weather"],
+		["VOL:Compacted Demand", "traffic management initiatives"],
+		["VOL:Multi-taxi", "traffic management initiatives"],
+		["VOL:Volume", "high traffic volume"]
+	];
+
+	test.each(tests)("parts(%p) === %p", (input, expected) => {
+		const reason = new Reason(input as string);
+		expect(reason.toString()).toStrictEqual(expected);
+	});
+});
--- src/types/Reason.ts
+++ src/types/Reason.ts
@@ -1,3 +1,10 @@
+enum ParentType {
+	WEATHER = "WX",
+	TRAFFIC_MANAGEMENT = "TM Initiatives",
+	RUNWAY = "RWY",
+	VOLUME = "VOL",
+}
+
 export class Reason {
 	#raw: string;
 
@@ -5,31 +12,68 @@ export class Reason {
 		this.#raw = raw;
 	}
 
+	parts(): string[] {
+		return this.#raw.split(":");
+	}
+
+	parentType(): ParentType | undefined {
+		const values = Object.values(ParentType);
+
+		if (values.includes(this.parts()[0] as ParentType)) {
+			return this.parts()[0] as ParentType;
+		} else {
+			return undefined;
+		}
+	}
+
 	/**
 	 * This returns a human friendly readable string for the given reason. If the reason is unable to be parsed into a human friendly string, this returns undefined.
 	 */
 	toString(): string | undefined {
-		switch (this.#raw) {
-			case "WX:Fog":
-				return "fog";
-			case "WX:Low Ceilings":
-				return "weather";
-			case "WX:Thunderstorms":
-				return "thunderstorms";
-			case "RWY:Noise Abatement":
-				return "noise reduction measures";
-			case "RWY:Maintenance":
-				return "runway maintenance";
-			case "RWY:Construction":
-				return "runway construction";
-			case "TM Initiatives:MIT:VOL":
-			case "VOL:Compacted Demand":
-			case "VOL:Multi-taxi":
-				return "traffic management initiatives";
-			case "TM Initiatives:MIT:WX":
-				return "weather";
-			case "VOL:Volume":
-				return "high traffic volume";
+		const subParts = this.parts().slice(1);
+		const subPartsStr = subParts.join(":");
+
+		switch (this.parentType()) {
+			case ParentType.WEATHER:
+				switch(subPartsStr) {
+					case "Fog":
+						return "fog";
+					case "Low Ceilings":
+						return "low ceilings";
+					case "Thunderstorms":
+						return "thunderstorms";
+					default:
+						return "weather";
+				}
+			case ParentType.TRAFFIC_MANAGEMENT:
+				switch(subPartsStr) {
+					case "MIT:VOL":
+						return "traffic management initiatives";
+					case "MIT:WX":
+						return "weather";
+					default:
+						return "traffic management initiatives";
+				}
+			case ParentType.RUNWAY:
+				switch(subPartsStr) {
+					case "Noise Abatement":
+						return "noise reduction measures";
+					case "Maintenance":
+						return "runway maintenance";
+					case "Construction":
+						return "runway construction";
+					default:
+						return undefined;
+				}
+			case ParentType.VOLUME:
+				switch(subPartsStr) {
+					case "Compacted Demand":
+					case "Multi-taxi":
+						return "traffic management initiatives";
+					case "Volume":
+					default:
+						return "high traffic volume";
+				}
 			default:
 				return undefined;
 		}
