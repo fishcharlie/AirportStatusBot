85518bafa6906cad501693f3d3dfea1c19188666
Charlie Fish <fishcharlie@me.com>
2023-12-15 20:01:01 -0700

    Moving Reason to new file

--- src/types/Status.ts
+++ src/types/Status.ts
@@ -3,6 +3,7 @@ import { startsWithVowel } from "../utils/startsWithVowel";
 import { Airport } from "./Airport";
 import * as luxon from "luxon";
 import { find as findTZ } from "geo-tz";
+import { Reason } from "./Reason";
 
 export class Status {
 	airportCode: string;
@@ -194,44 +195,6 @@ class Type {
 				return "Airport_Closure_List.Airport";
 			case TypeEnum.DELAY:
 				return "Arrival_Departure_Delay_List.Delay";
-			default:
-				return undefined;
-		}
-	}
-}
-
-class Reason {
-	#raw: string;
-
-	constructor(raw: string) {
-		this.#raw = raw;
-	}
-
-	/**
-	 * This returns a human friendly readable string for the given reason. If the reason is unable to be parsed into a human friendly string, this returns undefined.
-	 */
-	toString(): string | undefined {
-		switch (this.#raw) {
-			case "WX:Fog":
-				return "fog";
-			case "WX:Low Ceilings":
-				return "weather";
-			case "WX:Thunderstorms":
-				return "thunderstorms";
-			case "RWY:Noise Abatement":
-				return "noise reduction measures";
-			case "RWY:Maintenance":
-				return "runway maintenance";
-			case "RWY:Construction":
-				return "runway construction";
-			case "TM Initiatives:MIT:VOL":
-			case "VOL:Compacted Demand":
-			case "VOL:Multi-taxi":
-				return "traffic management initiatives";
-			case "TM Initiatives:MIT:WX":
-				return "weather";
-			case "VOL:Volume":
-				return "high traffic volume";
 			default:
 				return undefined;
 		}
