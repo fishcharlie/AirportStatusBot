b0619e5efcd14b3e21164148c2fe24d933f54103
Charlie Fish <contact@charlie.fish>
2023-12-21 11:03:53 -0700

    Adding more tests
    Signed-off-by: Charlie Fish <contact@charlie.fish>

--- src/types/Status.test.ts
+++ src/types/Status.test.ts
@@ -23,6 +23,44 @@ describe("Status.fromRAW().toPost()", () => {
 				}
 			},
 			"A departure delay has been issued for Test Airport A (#AAA) due to traffic management initiatives. Current delays are 46-60 minutes and increasing."
+		],
+		[
+			{
+				"Name": "Airport Closures",
+				"Airport_Closure_List": {
+					"Airport": {
+						"ARPT": "AAA",
+						"Reason": "!AAA 09/001 AAA AIRPORT CLSD 2109010000-2109012359",
+						"Start": "Dec 13 at 18:00 UTC.",
+						"Reopen": "Dec 13 at 23:59 UTC."
+					}
+				}
+			},
+			"An airport closure has been issued for Test Airport A (#AAA). The airport is expected to reopen December 12 at 4:59 PM."
+		],
+		[
+			{
+				"Name": "Ground Delay Programs",
+				"Ground_Delay_List": {
+					"Ground_Delay": {
+						"ARPT": "AAA",
+						"Reason": "TM Initiatives:MIT:VOL"
+					}
+				}
+			},
+			"Inbound aircraft to Test Airport A (#AAA) are currently being delayed at their origin airport due to traffic management initiatives. It is currently unknown how long the delays are."
+		],
+		[
+			{
+				"Name": "Ground Stop Programs",
+				"Ground_Stop_List": {
+					"Program": {
+						"ARPT": "AAA",
+						"Reason": "TM Initiatives:MIT:VOL"
+					}
+				}
+			},
+			"Inbound aircraft to Test Airport A (#AAA) are currently being held at their origin airport due to traffic management initiatives. It is currently unknown when operations will resume."
 		]
 	];
 
