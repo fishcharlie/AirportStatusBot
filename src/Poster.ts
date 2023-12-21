8fd704eca399b0ebbe06904af1a6167b51515819
Charlie Fish <contact@charlie.fish>
2023-12-21 11:04:14 -0700

    Adding reply method in Poster
    Signed-off-by: Charlie Fish <contact@charlie.fish>

--- src/Poster.ts
+++ src/Poster.ts
@@ -1,7 +1,8 @@
 import { Config, ContentTypeEnum, SocialNetwork, defaultIncludeHashtags } from "./types/Config";
 import { S3 } from "@aws-sdk/client-s3";
 import TuskMastodon from "tusk-mastodon";
-import { BskyAgent, RichText } from "@atproto/api";
+import { BskyAgent, RichText, AppBskyFeedPost } from "@atproto/api";
+import { GeneralObject } from "js-object-utilities";
 
 export class Poster {
 	#config: Config;
@@ -82,6 +83,97 @@ export class Poster {
 						returnObject[socialNetwork.uuid] = {
 							key
 						};
+						break;
+					default:
+						throw new Error(`Unknown social network (${socialNetwork.name}): ${socialNetwork.type}`);
+				}
+			} catch (e) {
+				console.error(e);
+			}
+		}));
+
+		return returnObject;
+	}
+
+	async reply(replyTo: GeneralObject<any>, message: string, rawXML: string, contentTypes: ContentTypeEnum[]): Promise<{ [key: string]: any }> {
+		let returnObject: { [key: string]: any } = {};
+
+		await Promise.all(this.#config.socialNetworks.map(async (socialNetwork) => {
+			if (!contentTypes.includes(socialNetwork.contentType)) {
+				console.log(`Skipping ${socialNetwork.name} for post ('${message}') because it doesn't support ${contentTypes.join(", ")}.`);
+				return;
+			}
+
+			const socialMessage = this.#formatMessage(message, socialNetwork);
+
+			try {
+				switch (socialNetwork.type) {
+					case "mastodon":
+						// const mastodon = new TuskMastodon({
+						// 	"api_url": `${socialNetwork.credentials.endpoint}/api/v1/`,
+						// 	"access_token": socialNetwork.credentials.password,
+						// 	"timeout_ms": 60 * 1000,
+						// });
+						// const mastodonResult = await mastodon.post("statuses", {
+						// 	"status": socialMessage,
+						// 	"in_reply_to_id": replyTo.data.id,
+						// });
+						// returnObject[socialNetwork.uuid] = mastodonResult;
+						break;
+					case "bluesky":
+						// const bluesky = new BskyAgent({
+						// 	"service": socialNetwork.credentials.endpoint
+						// });
+
+						// await bluesky.login({
+						// 	"identifier": socialNetwork.credentials.username ?? "",
+						// 	"password": socialNetwork.credentials.password ?? ""
+						// });
+
+						// const rt = new RichText({
+						// 	"text": socialMessage
+						// });
+						// await rt.detectFacets(bluesky);
+						// const postRecord: Partial<AppBskyFeedPost.Record> & Omit<AppBskyFeedPost.Record, "createdAt"> = {
+						// 	"text": rt.text,
+						// 	"facets": rt.facets,
+						// 	"reply": {
+						// 		"root": {
+						// 			"uri": replyTo.root.uri,
+						// 			"cid": replyTo.root.cid
+						// 		},
+						// 		"parent": {
+						// 			"uri": replyTo.parent.uri,
+						// 			"cid": replyTo.parent.cid
+						// 		}
+						// 	}
+						// };
+						// const blueskyResult = await bluesky.post(postRecord);
+						// returnObject[socialNetwork.uuid] = {
+						// 	"root": replyTo.root,
+						// 	"parent": blueskyResult
+						// };
+						break;
+					case "s3":
+						const client = new S3({
+							"credentials": {
+								"accessKeyId": socialNetwork.credentials.accessKeyId,
+								"secretAccessKey": socialNetwork.credentials.secretAccessKey
+							},
+							"region": "us-west-2"
+						});
+						const key = replyTo.key;
+
+						const existingContent = await client.getObject({
+							"Bucket": socialNetwork.credentials.bucket,
+							"Key": key
+						});
+						const existingContentString = await existingContent.Body?.transformToString();
+						await client.putObject({
+							"Bucket": socialNetwork.credentials.bucket,
+							"Body": `${existingContentString}\n\n\n\n\n\n<><><><><>\n\n\n\n\n\n` + `${socialMessage}${socialNetwork.settings?.includeRAWXML ? `\n\n---\n\n${rawXML}` : ""}`,
+							"Key": key
+						});
 						break;
 					default:
 						throw new Error(`Unknown social network (${socialNetwork.name}): ${socialNetwork.type}`);
