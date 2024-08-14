97a77cce7a72f1ec82ba732d3d923b0ad1a1d23e
Charlie Fish <contact@charlie.fish>
2024-08-14 17:59:08 -0600

    Throwing error for invalid CSV ourairports
    Signed-off-by: Charlie Fish <contact@charlie.fish>

--- src/OurAirportsDataManager.ts
+++ src/OurAirportsDataManager.ts
@@ -58,6 +58,9 @@ export class OurAirportsDataManager {
 						"User-Agent": this.userAgent
 					}
 				})).text();
+				if (!csvResult.startsWith(`"id","ident","type","name","latitude_deg","longitude_deg","elevation_ft","continent","iso_country","iso_region","municipality","scheduled_service","gps_code","iata_code","local_code","home_link","wikipedia_link","keywords"`)) {
+					throw new Error("Invalid CSV data: " + csvResult);
+				}
 				await fs.promises.mkdir(path.join(__dirname, "..", "cache", "ourairports"), {
 					"recursive": true
 				});
