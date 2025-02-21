5eced3b1488815890244ffd9065b58fa69c272e9
Charlie Fish <contact@charlie.fish>
2025-02-21 09:02:03 -0700

    Fixing change to OurAirports format
    Signed-off-by: Charlie Fish <contact@charlie.fish>

--- src/OurAirportsDataManager.ts
+++ src/OurAirportsDataManager.ts
@@ -59,7 +59,7 @@ export class OurAirportsDataManager {
 						"User-Agent": this.userAgent
 					}
 				})).text();
-				if (!csvResult.startsWith(`"id","ident","type","name","latitude_deg","longitude_deg","elevation_ft","continent","iso_country","iso_region","municipality","scheduled_service","gps_code","iata_code","local_code","home_link","wikipedia_link","keywords"`)) {
+				if (!csvResult.startsWith(`"id","ident","type","name","latitude_deg","longitude_deg","elevation_ft","continent","iso_country","iso_region","municipality","scheduled_service","icao_code","iata_code","gps_code","local_code","home_link","wikipedia_link","keywords"`)) {
 					throw new Error("Invalid CSV data: " + csvResult);
 				}
 				await fs.promises.mkdir(path.join(__dirname, "..", "cache", "ourairports"), {
